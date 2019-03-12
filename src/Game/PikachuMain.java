8fa05d13aabdf3129964ef096f40469ad6b46dea
namtran166 <48459680+namtran166@users.noreply.github.com>
2019-03-11 20:04:00 -0400

    Upload files to this new account

---
+++ src/Game/PikachuMain.java
@@ -0,0 +1,34 @@
+package Game;
+
+public class PikachuMain {
+	PikachuFrame frame;
+
+	public PikachuMain() {
+		frame = new PikachuFrame();
+		MyTimeCount timeCount = new MyTimeCount();
+		timeCount.start();
+		new Thread(frame).start();
+	}
+
+	public static void main(String[] args) {
+		new PikachuMain();
+	}
+
+	class MyTimeCount extends Thread {
+
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				frame.setTime(frame.getTime() - 1);
+				if (frame.getTime() == 0) {
+					frame.showDialogNewGame(
+							"Full time\nDo you want play again?", "Lose");
+				}
+			}
+		}
+	}
+}
